ad9e24dc476824626ff19268f5d6908b44f55a6e
ZakZanko <zakzanko2112@gmail.com>
2023-07-12 16:47:49 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,68 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+import Phonebook from './Phonebook/Phonebook';
+import ContactForm from './ContactForm/ContactForm';
+import Contacts from './Contacts/Contacts';
+
+class App extends Component {
+  state = {
+    contacts: [],
+  };
+
+  handleAddContact = newContact => {
+    const { contacts } = this.state;
+    this.setState({ contacts: [...contacts, newContact] });
+  };
+
+  handlerUniqName = name => {
+    const { contacts } = this.state;
+    const uniqName = !!contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+    if (uniqName) {
+      alert(`${name} is already in contacts`);
+      return false;
+    }
+    return true;
+  };
+
+  handleDeleteContact = id => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(contact => contact.id !== id),
+      };
+    });
+  };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  render() {
+    const { contacts } = this.state;
+    return (
+      <Phonebook title="Phonebook">
+        <ContactForm
+          onAdd={this.handleAddContact}
+          onCheckforUniqName={this.handlerUniqName}
+        />
+        <Contacts
+          title="Contacts"
+          contacts={contacts}
+          onDeleteContact={this.handleDeleteContact}
+        />
+      </Phonebook>
+    );
+  }
+}
+
+export default App;
